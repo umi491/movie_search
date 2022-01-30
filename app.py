5fbf9cc1f1196330aa2c20f98a76894454063df9
Michelle <ljleem5475@gmail.com>
2022-01-30 18:59:18 +1100

    version using omdb api

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 import requests
-import configparser
-from flask import Flask
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def movie_dashboard():
+def search_home():
     return render_template('home.html')
 
-def get_movie_results(movie_title):
-    api_url = "https://imdb-api.com/API/SearchAl/k_yz9612sh/?title={}".format(movie_title)
-    print(api_url)
 
-get_movie_results("inception")
+@app.route('/results', methods=['POST'])
+def search_results():
+    movie_title = request.form['movieTitle']
+    data = get_movies(movie_title)
+
+    title = data["Title"]
+    released = data["Released"]
+    genre = data["Genre"]
+
+    return render_template('results.html', title=title, released=released, genre=genre)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+def get_movies(movie_title):
+    api_url = "http://www.omdbapi.com/?t={}&apikey=3507e773".format(movie_title)
+    r = requests.get(api_url)
+    return r.json()
---
+++ readme.txt
@@ -0,0 +1,5 @@
+A simple web application which takes in search terms and returns a list of movies
+
+TO-DO
+[ ] Implement watchlist features (watched, plan to watch, favourites)
+[ ] Implement ability to give each movie a rating and review
