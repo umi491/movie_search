0d68ff1b11c98b097e434eb46e0d12de2ad30b9b
Michelle <ljleem5475@gmail.com>
2022-01-31 13:02:47 +1100

    test branch using omdb api

--- app.py
+++ app.py
@@ -17,9 +17,12 @@ def search_results():
     title = data["Title"]
     released = data["Released"]
     genre = data["Genre"]
+    rating = []
+    for Ratings in data['Ratings']:
+        rating.append(Ratings['Source'])
+        rating.append(Ratings['Value'])
 
-    return render_template('results.html', title=title, released=released, genre=genre)
-
+    return render_template('results.html', title=title, released=released, genre=genre, rating=rating)
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -27,5 +30,6 @@ if __name__ == '__main__':
 
 def get_movies(movie_title):
     api_url = "http://www.omdbapi.com/?t={}&apikey=3507e773".format(movie_title)
+    # api_url = "https://imdb-api.com/en/API/SearchMovie/k_yz9612sh/{}".format(movie_title)
     r = requests.get(api_url)
     return r.json()
